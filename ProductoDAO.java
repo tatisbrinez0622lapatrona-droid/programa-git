2ca1cedcf2a0323bff9ec46806684207ecf1ec2b
Sara Briñez <tatisbrinez0622.lapatrona@gmail.com>
2026-04-29 16:36:48 -0500

    Codificación web lista

--- ProductoDAO.java
+++ ProductoDAO.java
@@ -3,3 +3,6 @@ public class ProductoDAO {
         System.out.println("Guardando prenda en la base de datos...");
     }
 }
+public class ProductoDAO {
+    // Aquí irá la lógica para Insertar, Consultar y Borrar ropa
+}
